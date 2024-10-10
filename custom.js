b84a48012c35dfa98956e429842cc545fa1c4f05
SaveliyVotintsev <181251436+SaveliyVotintsev@users.noreply.github.com>
2024-10-10 11:57:16 +0000

    Deploying to gh-pages from @ SaveliyVotintsev/MoS@f103df821103456a2d487fd748f24a0165a2e9ef 🚀

---
+++ custom.js
@@ -0,0 +1,74 @@
+﻿function scrollToElement(elementId) {
+    const element = document.getElementById(elementId);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "center"
+        });
+    }
+}
+
+let chart;
+
+function createChart(functionString, step, maxT) {
+    const ctx = document.getElementById('myChart');
+    const data = generateData(functionString, step, maxT);
+
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.labels,
+            datasets: [{
+                label: 'h(t)',
+                data: data.values,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                pointStyle: false
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+}
+
+function updateChart(functionString, step, maxT) {
+    if (chart) {
+        const data = generateData(functionString, step, maxT);
+        chart.data.labels = data.labels;
+        chart.data.datasets[0].data = data.values;
+        chart.update();
+    } else {
+        createChart(functionString, step, maxT);
+    }
+}
+
+function deleteChart() {
+    if (chart) {
+        chart.destroy();
+        chart = null;
+    }
+}
+
+function generateData(functionString, step, maxT) {
+    const labels = [];
+    const values = [];
+
+    for (let t = 0; t <= maxT; t += step) {
+        labels.push(t.toFixed(6));
+        values.push(calculateFunction(functionString, t));
+    }
+
+    return { labels, values };
+}
+
+function calculateFunction(functionString, t) {
+    const func = new Function('t', 'return ' + functionString.replace(/exp/g, 'Math.exp').replace(/cos/g, 'Math.cos'));
+    return func(t);
+}
